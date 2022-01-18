4f2c4b5b2673a6995c5f52d88a71850de351006a
AfrikanFrog <1andrewwright@gmail.com>
2022-01-18 09:47:08 -0700

    Update

---
+++ Capstone2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using a dataset from Craigslist, explore data insights of used cars and create a sales indicator of car brand based on location.',
+    author='Andrew Wright',
+    license='',
+)
